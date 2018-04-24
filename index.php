0e06c03f1c498b4399492f07872a06a4d49a55b9
Matthew Scharf <msxfd@mst.edu>
2018-04-23 20:27:27 -0500

    Added index and main style sheet

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="mainstyle.css">
+	</head>
+	<body>
+		<div id="nav-bar">
+			
+		</div>
+		<?php
+			phpinfo();
+		?>
+	</body>
+</html>
